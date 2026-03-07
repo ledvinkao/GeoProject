7dfd33ae1ba9fa515f462f78ed0b4ec279610eb2
Ondrej Ledvinka <stathydro.cz@gmail.com>
2026-03-07 17:40:39 +0100

    script 01 updated to reflect current situation and knowledge

--- scripts/01_gee_nahrani_zajmoveho_polygonu_pres_api.R
+++ scripts/01_gee_nahrani_zajmoveho_polygonu_pres_api.R
@@ -7,20 +7,21 @@ xfun::pkg_attach2("tidyverse",
                   "googledrive",
                   "rgee")
 
-# následující odkaz by měl být stejný pro všechny a setrvávat, dokud sdílený ZIP soubor v Classroomu neodstraní
+# následující odkaz by měl být stejný pro všechny a setrvávat, dokud sdílený ZIP soubor nebude z Classroomu odstraněn
 url <- "https://drive.google.com/file/d/1gvCm4pqgMQObUMdvi8TOgKbGcoySQklC/view?usp=sharing"
 
 # vytváříme dočasný ZIP soubor, který zmizí po restartu R sezení
 tmp <- tempfile(fileext = ".zip")
 
-# plníme dočasný ZIP soubor stažením; z něj si pak vezmeme bez rozbalení polygon Milešovského potoka
+# plníme dočasný ZIP soubor stažením
+# z něj si pak vezmeme bez rozbalení polygon Milešovského potoka
 drive_download(url,
                path = tmp)
 
 # zjistímě, jaké všechny vrstvy máme v archivu
-st_layers(str_c("/vsizip/",
+st_layers(str_c("/vsizip/", # namísto str_c() je možné používat i funkci str_glue()
                 tmp,
-                "/Milesovsky_GIS")) # bohužel tohle musíme do cesty dodat, protože v souboru je ještě další složka
+                "/Milesovsky_GIS")) # tohle musíme do cesty dodat, protože v souboru je ještě další složka
 
 # a vybereme si jen polygon povodí
 catch <- read_sf(str_c("/vsizip/",
@@ -31,19 +32,21 @@ catch <- read_sf(str_c("/vsizip/",
 # připojujeme se k GEE
 # specifikujeme uživatele i možnost práce přes Drive
 # každý bude mít svoje údaje
-ee_Initialize(user = "ledvinka@natur.cuni.cz",
-              drive = T)
+ee_Initialize(user = "ledvinka@natur.cuni.cz")
 
 # nastavujeme název assetu v GEE
-# inspirace nápovědou k funkci sf_as_ee()
-assetID <- sprintf("%s/%s", 
-                   ee_get_assethome(), 
-                   'milesovsky')
+# adresář můžeme nastavit podle toho, co vidíme u svého profilu v https://code.earthengine.google.com/
+assetID <- str_c("users/ledvinka/", 
+                 'milesovsky')
 
 # nahrajeme polygon do GEE
 # a rovnou si vytváříme objekt, který na asset odkazuje
-catch_ee <- sf_as_ee(
+catch_ee <- sf_as_ee( # takto se vlastně ihned polygon přiřadí k objektu, ale jinak lze kopírovat cesty z assetů
   x = catch,
-  overwrite = TRUE,
+  overwrite = TRUE, # v případě, že vrstva se stejným názvem již existuje, ji přepíšeme
   assetId = assetID,
-  via = "getInfo_to_asset")
+  via = "getInfo_to_asset") # abychom vložili polygon povodí mezi ostatní assety
+
+# může se objevit chyba, ale nakonec se polygon nahrát podaří, o čemž se můžeme snadno přesvědčit prohlížením assetů
+
+# nakonec můžeme nové vrstvě v GEE dodat popisek atd.
